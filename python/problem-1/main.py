41e4d5186e04b5dd8324734a03721f43edbe8fb8
Evan <evan.allender@gmail.com>
2026-01-29 18:00:16 -0500

    euler(python): problem 1

---
+++ python/problem-1/main.py
@@ -0,0 +1,14 @@
+def main():
+    total = 0
+    index = 1000
+    while index:
+        index -= 1
+        if not (index % 3) or not (index % 5):
+            total += index
+
+    print(total)
+    assert total == 233168
+
+
+if __name__ == "__main__":
+    main()
